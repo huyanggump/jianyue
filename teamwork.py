52003c7ddb29728a87ab5efa430d5c6a71d68d86
scissorsy <scissorsy@163.com>
2014-09-21 10:31:32 +0800

    4 week dby

--- teamwork.py
+++ teamwork.py
@@ -1,10 +1,10 @@
 __author__ = 'boyang'
 #################Junhui's code here#####
-print('I have complete my task')
+
 ########################################
 
 #################Yongran's code here####
-print('I have complete my task')
+
 ########################################
 
 #################boyang's code here#####
